37d4285f2644bea6b8ff2d169edccb739e411a3b
Rafael Cruz <rcruz@Rafaels-MacBook-Air.local>
2016-06-18 15:08:36 -0300

    Adding Project

---
+++ GitHubApi/GitHubApi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json;
+
+namespace GitHubApi
+{
+	public class GitHubApi
+	{
+		private const string GitHubUserRepositoryUrl = "https://api.github.com/users/{0}/repos";
+
+
+		public async Task<List<string>> GetUserRepository(String user)
+		{
+			var client = new HttpClient();
+			var response = await client.GetAsync(String.Format(GitHubUserRepositoryUrl, user));
+			var json = response.Content.ReadAsStringAsync().Result;
+
+			var result = JsonConvert.DeserializeObject<List<String>>(json);
+
+			return result;
+		}
+	}
+}
+
---
+++ HelloWorld/MainActivity.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace HelloWorld
+{
+	[Activity(Label = "HelloWorld", MainLauncher = true )]
+	public class MainActivity : Activity
+	{
+		protected int count = 0;
+
+		protected GitHubApi.GitHubApi api = new GitHubApi.GitHubApi(); 
+
+		protected override void OnCreate(Bundle savedInstanceState)
+		{
+			base.OnCreate(savedInstanceState);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			Button helloWordBnt = FindViewById<Button>(Resource.Id.BtnClick);
+
+
+
+			helloWordBnt.Click +=  async (object sender, System.EventArgs e) =>
+			{
+				var repositories = await api.GetUserRepository("rafaelcruz-net");
+
+
+
+			};
+
+		}
+	}
+}
+
+
